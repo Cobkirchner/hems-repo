9e1cbe285c6cde703004cacc08864efa28adadee
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-07 21:34:24 +0100

    update

--- database_connection.ps1
+++ database_connection.ps1
@@ -1,12 +1,12 @@
-﻿[void][System.Reflection.Assembly]::LoadFrom("/home/mysql-connector-net/v4.0/MySql.Data.dll")
-
+﻿#[void][System.Reflection.Assembly]::LoadFrom("/home/mysql-connector-net/v4.0/MySql.Data.dll")
+[void][System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
 #Initiate the database connection
 $db_server="localhost"
 $db_user="root"
 $db_password="7S2Dh972m8k535"
 $database="hems"
 $db_connection = New-Object MySql.Data.MySqlClient.MySqlConnection
-$db_connection.ConnectionString = "server=$db_server;user id=$db_user;password=$db_password;database=$database;pooling=false"
+$db_connection.ConnectionString = "server=$db_server;uid=$db_user;pwd=$db_password;database=$database;"
 $db_connection.Open()
 
 
