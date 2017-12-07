f6d7eb96f61056dea9093fa224609704f13f2688
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-07 21:26:46 +0100

    update

--- database_connection.ps1
+++ database_connection.ps1
@@ -1,4 +1,4 @@
-﻿[void][System.Reflection.Assembly]::LoadFrom("/home/mysql-connector-net/v4.5/MySql.Data.dll")
+﻿[void][System.Reflection.Assembly]::LoadFrom("/home/mysql-connector-net/v4.0/MySql.Data.dll")
 
 #Initiate the database connection
 $db_server="localhost"
