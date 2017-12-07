09ac4e9942a2f48a134b61bd6f31d132ec12fed2
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-07 22:28:36 +0100

    added python

---
+++ database.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="hems_user",         # your username
+                     passwd="nF4mTRDT69RySz",  # your password
+                     db="hems")        # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cursor = db.cursor()
+
+# Use all the SQL you like
+cursor.execute("SELECT * participants")
+
+# print all the first cell of all the rows
+for row in cursor.fetchall():
+    print row[0]
+
+db.close()
