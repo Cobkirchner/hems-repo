a6732bc0e84d9ef3ae45dfc0846c698c1e8a78a8
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-08 19:45:37 +0100

    added try catch

--- database.py
+++ database.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import Mysqldb;
-#import mysql;
-#import mysql.connector;
+import mysql.connector
+from mysql.connector import Error
+ 
+ 
+def connect():
+    """ Connect to MySQL database """
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='hems',
+                                       user='hems_user',
+                                       password='nF4mTRDT69RySz')
+        if conn.is_connected():
+            print('Connected to MySQL database')
+ 
+    except Error as e:
+        print(e)
+ 
+    finally:
+        conn.close()
+ 
+ 
+if __name__ == '__main__':
+    connect()
 
-# Verbindung erstellen
-try:
-    connection = Mysqldb.connect(host="localhost",    # your host, usually localhost
-                     user="hems_user",         # your username
-                     passwd="nF4mTRDT69RySz",  # your password
-                     db="hems")        # name of the data base
-except:
-    print("Keine Verbindung zum Server")
-    exit(0)
+
 
 # Datensätze auslesen
-cursor = connection.cursor()
-cursor.execute("SELECT * from participants")
-result = cursor.fetchall()
-cursor.close()
+#cursor = connection.cursor()
+#cursor.execute("SELECT * from participants")
+#result = cursor.fetchall()
+#cursor.close()
 
-for data in result:
-    print(str(data[0]) + data[1] + data[2])
+#for data in result:
+#    print(str(data[0]) + data[1] + data[2])
