cd1700d6de814a6fb07cd1bda877906ea74d2d4f
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-08 22:35:02 +0100

    test

--- read_from_cli.py
+++ read_from_cli.py
@@ -2,17 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
-
-def read_from_cli(surname, name, email, password):
-    surname = input('Please enter surname: ')
-    name = input('Please enter name: ')
-    email = input('Please enter email: ')
-    password = input('Please enter password: ')
-    print "You entered:", surname, name, email, password
-
-def main():
-   read_from_cli(surname, name, email, password)
- 
-if __name__ == '__main__':
-    main()
+surname = input('Please enter surname: ')
+name = input('Please enter name: ')
+email = input('Please enter email: ')
+password = input('Please enter password: ')
+print "You entered:", ´str(surname), str(name), str(email), str(password)
