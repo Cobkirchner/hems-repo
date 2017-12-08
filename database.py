9632145e11b1bcd7e8d2109af77e914d49d0fb84
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-08 18:39:58 +0100

    up

--- database.py
+++ database.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
-import MySQLdb
 
-db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+import mysql;
+import mysql.connector;
+
+#Verbindung erstellen
+try:
+    connection = mysql.connector.connect(host="localhost",    # your host, usually localhost
                      user="hems_user",         # your username
                      passwd="nF4mTRDT69RySz",  # your password
                      db="hems")        # name of the data base
+except:
+    print "Keine Verbindung zum Server"
+    exit(0)
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cursor = db.cursor(MySQLdb.cursors.DictCursor)
+#Datensätze auslesen
+cursor = connection.cursor()
+cursor.execute("SELECT * from participants")
+result = cursor.fetchall()
+cursor.close()
 
-# Use all the SQL you like
-cursor.execute("SELECT * FROM participants")
-
-# print all the first cell of all the rows
-#for row in cursor.fetchall():
-#    print "%s, %s" % (row["participants_id"], row["participants_email"], row["participants_password"])
-
-for (participants_name, participants_email, participants_password) in cursor:
-      print("{}, {}, {}".format(participants_name, participants_email, participants_password))
-
-db.close()
+for data in result:
+    print str(data[0]) + data[1] + data[2]
