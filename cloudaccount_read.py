ba13236b4f021ecedd7717e0fab2c92ee6db80b2
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-12 19:39:34 +0100

    update

---
+++ cloudaccount_read.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
+ 
+ 
+def cloudaccount_read():
+    try:
+        dbconfig = read_db_config()
+        conn = MySQLConnection(**dbconfig)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM cloudaccount")
+        results = cursor.fetchall()
+ 
+        widths = []
+        columns = []
+        tavnit = '|'
+        separator = '+' 
+
+        for cd in cursor.description:
+            widths.append(max(cd[2], len(cd[0])))
+            columns.append(cd[0])
+
+        for w in widths:
+            tavnit += " %-"+"%ss |" % (w,)
+            separator += '-'*w + '--+'
+
+        print(separator)
+        print(tavnit % tuple(columns))
+        print(separator)
+        for row in results:
+            print(tavnit % row)
+        print(separator)
+
+
+
+    except Error as e:
+        print(e)
+ 
+    finally:
+        cursor.close()
+        conn.close()
+ 
+ 
+
+
+
+if __name__ == '__main__':
+   cloudaccount_read()
