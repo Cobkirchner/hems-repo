f8a1d00dcaef55f23a46232dfc7a58b4324e475b
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-18 06:31:33 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,12 +28,12 @@ resource "azurerm_subnet" "subnet" {
 
 resource "azurerm_network_interface" "nic" {
   count               = "${var.instance_count}"  
-  name                = "nic${count.index}"
+  name                = "hypervcontainer_nic${count.index}"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 
   ip_configuration {
-    name                          = "ipconfig${count.index}"
+    name                          = "hypervcontainer_ipconfig${count.index}"
     subnet_id                     = "${azurerm_subnet.subnet.id[count.index]}"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = "${element(azurerm_public_ip.pip.*.id, count.index)}"
@@ -42,23 +42,23 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_public_ip" "pip" {
   count               = "${var.instance_count}"  
-  name                         = "pip${count.index}"
+  name                         = "hypervcontainer_pip${count.index}"
   location                     = "${var.location}"
   resource_group_name          = "${azurerm_resource_group.rg.name}"
   public_ip_address_allocation = "Dynamic"
-  domain_name_label            = "pip${count.index}"
+  domain_name_label            = "hypervcontainer_pip${count.index}"
 }
 
 resource "azurerm_virtual_machine" "vm" {
   count               = "${var.instance_count}"
-  name                  = "vm${count.index}"
+  name                  = "hypervcontainer_${count.index}"
   location              = "${var.location}"
   resource_group_name   = "${azurerm_resource_group.rg.name}"
   vm_size               = "${var.vm_size}"
   network_interface_ids = ["${element(azurerm_network_interface.nic.*.id, count.index)}"]
 
   storage_os_disk {  
-    name          = "osdisk${count.index}"
+    name          = "hypervcontainer_osdisk${count.index}"
     image_uri     = "${var.image_uri}"
     vhd_uri       = "https://hemsstorage.blob.core.windows.net/hemscontainer/hyperv-container.vhd"
     os_type       = "${var.os_type}"
@@ -67,7 +67,7 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   os_profile {  
-    computer_name  = "osprofile${count.index}"
+    computer_name  = "hypervcontainer_osprofile${count.index}"
     admin_username = "${var.admin_username}"
     admin_password = "${var.admin_password}"
   }
