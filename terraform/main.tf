82ddd504a10161adabccec58c648e8b7acddc645
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-18 04:50:49 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,12 +28,12 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = ["${element(var.hostname.*, count.index)}nic"]
+  name                = "${var.hostname.*}nic"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 
   ip_configuration {
-    name                          = ["${element(var.hostname.*, count.index)}ipconfig"]
+    name                          = "${var.hostname.*}ipconfig"
     subnet_id                     = "${azurerm_subnet.subnet.id}"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = "${azurerm_public_ip.pip.id}"
@@ -41,22 +41,22 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_public_ip" "pip" {
-  name                         = ["${element(var.hostname.*, count.index)}-ip"]
+  name                         = "${var.hostname.*}-ip"
   location                     = "${var.location}"
   resource_group_name          = "${azurerm_resource_group.rg.name}"
   public_ip_address_allocation = "Dynamic"
-  domain_name_label            = ["${element(var.hostname.*, count.index)}"]
+  domain_name_label            = "${var.hostname}"
 }
 
 resource "azurerm_virtual_machine" "vm" {
-  name                  = ["${element(var.hostname.*, count.index)}"]
+  name                  = "${var.hostname.*}"
   location              = "${var.location}"
   resource_group_name   = "${azurerm_resource_group.rg.name}"
   vm_size               = "${var.vm_size}"
   network_interface_ids = ["${azurerm_network_interface.nic.id}"]
 
   storage_os_disk {
-    name          = ["${element(var.hostname.*, count.index)}-osdisk1"]
+    name          = "${var.hostname.*}-osdisk1"
     image_uri     = "${var.image_uri}"
     vhd_uri       = "https://hemsstorage.blob.core.windows.net/hemscontainer/hyperv-container.vhd"
     os_type       = "${var.os_type}"
@@ -65,13 +65,12 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   os_profile {
-    computer_name  = ["${element(var.hostname.*, count.index)}"]
+    computer_name  = "${var.hostname.*}"
     admin_username = "${var.admin_username}"
     admin_password = "${var.admin_password}"
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
-    }
- 
+  }
 }
