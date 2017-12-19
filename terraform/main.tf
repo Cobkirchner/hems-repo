8a9fc75957ba77e257e4698dd38c8d1cd2a242a3
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-19 22:35:26 +0100

    up

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,8 +69,8 @@ resource "azurerm_virtual_machine" "vm" {
 
   os_profile {  
     computer_name  = "hypervcontainer-${count.index}"
-    admin_username = "${var.admin_username}"
-    admin_password = "${var.admin_password}"
+ #   admin_username = "${var.admin_username}"
+ #   admin_password = "${var.admin_password}"
   }
 
   os_profile_linux_config {
