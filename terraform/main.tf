d54d1a6477da070cd9db4ad64bfdc3af9cb2356e
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-20 02:42:09 +0100

    up

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ resource "azurerm_virtual_machine" "vm" {
     storage_os_disk {  
     name          = "hypervcon-osdisk${count.index}"
     image_uri     = "${var.image_uri}"
-    vhd_uri       = "https://hemsstorage.blob.core.windows.net/hemscontainer/hyperv-container${count.index}.vhd"
+    vhd_uri       = "https://hemsstorage.blob.core.windows.net/hemscontainer/hvc_osdisk${count.index}.vhd"
     os_type       = "${var.os_type}"
     caching       = "ReadWrite"
     create_option = "FromImage"
