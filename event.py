ab8b23f2261d2f19f7d367748b004608d84d37c8
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-20 20:49:56 +0100

    up

--- event.py
+++ event.py
@@ -61,8 +61,8 @@ def event_create_insert_into_db(name, type, num_participants, startdatetime, end
         cursor.execute(query, args)
         
         last_event_id = cursor.lastrowid
-        sql_event_start = "CREATE EVENT" + name + str(last_event_id) + startdatetime + "ON SCHEDULE AT '" + startdatetime + "' DO UPDATE hems.event SET state = 'ready' WHERE id =" + last_event_id + ";"
-        sql_event_end = "CREATE EVENT" + name + str(last_event_id) + enddatetime + "ON SCHEDULE AT '" + enddatetime + "' DO UPDATE hems.event SET state = 'deprovison' WHERE id =" + last_event_id + ";"
+        sql_event_start = "CREATE EVENT" + name + str(last_event_id) + startdatetime + "ON SCHEDULE AT '" + startdatetime + "' DO UPDATE hems.event SET state = 'ready' WHERE id =" + str(last_event_id) + ";"
+        sql_event_end = "CREATE EVENT" + name + str(last_event_id) + enddatetime + "ON SCHEDULE AT '" + enddatetime + "' DO UPDATE hems.event SET state = 'deprovison' WHERE id =" + str(last_event_id) + ";"
         
         cursor.execute(sql_event_start)
         cursor.execute(sql_event_end)
