cc7e5b6ba9f6fb85ae98be1f7cfcadb708d37883
Cobkirchner <christian.obkirchner@bpmspace.com>
2017-12-22 05:18:13 +0100

    u

--- instance_handler.py
+++ instance_handler.py
@@ -7,13 +7,13 @@ import sys
 from mysql.connector import MySQLConnection, Error
 from python_mysql_dbconfig import read_db_config
 
-def get_num_participants():
-    select_query = "SELECT num_participants FROM event WHERE state = "ready";"
+def get_num_participants(): 
+    
     try:
         dbconfig = read_db_config()
         conn = MySQLConnection(**dbconfig)
         cursor = conn.cursor()
-        cursor.execute(select_query)
+        cursor.execute("""SELECT num_participants FROM event WHERE state = "ready";""")
         result = cursor.fetchone()[0]()
 
         widths = []
@@ -42,3 +42,7 @@ def get_num_participants():
     finally:
         cursor.close()
         conn.close()
+
+#Main method
+if __name__ == '__main__':
+   get_num_participants()       
